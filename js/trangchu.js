86b0702a3f24a7a0e34908aa1b5afa338c9cafc8
ngath79 <maithinga7902@gmail.com>
2024-10-29 16:09:49 +0700

    init project

---
+++ js/trangchu.js
@@ -0,0 +1,14 @@
+// Back to Top Button
+const backToTopButton = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
